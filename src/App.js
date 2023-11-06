ebfd6005d2d9211c990695f24491b5f142345962
Sami Safatli <sami.safatli@yduqs.com.br>
2023-11-06 17:29:55 -0300

    Add App.js and index.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {ImageGenerator} from './Components/ImageGenerator/ImageGenerator'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <ImageGenerator/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
